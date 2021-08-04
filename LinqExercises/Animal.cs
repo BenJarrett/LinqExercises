ebaa71dc3d58c8f0101e9da034ed0311c37f394e
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-04 18:04:43 -0500

    Group By and some chaining

---
+++ LinqExercises/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqExercises
+{
+    internal class Animal
+    {
+        public string Type { get; internal set; }
+        public int HeightInInches { get; internal set; }
+        public int WeightInPounds { get; internal set; }
+    }
+}
